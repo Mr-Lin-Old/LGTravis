280e3ec389f9e36bd501fdb16a4ead5e3a354ffc
Kenny <zhongbiao.lin@livestar.com>
2020-03-03 16:24:29 +0800

    初始化

---
+++ LGTravis/LGTravis/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LGTravis
+//
+//  Created by Kenny on 2020/3/3.
+//  Copyright © 2020 Kenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LGTravis/LGTravis/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LGTravis
+//
+//  Created by Kenny on 2020/3/3.
+//  Copyright © 2020 Kenny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
